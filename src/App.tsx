3c02ba33a9ec6aae0dc442d290f2073a66699e09
Sergey73 <hripchenkosergey@rambler.ru>
2018-07-18 23:15:35 +0400

    added saas

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ class App extends React.Component {
   componentDidMount() {
     this.mapbox.accessToken = 'pk.eyJ1Ijoic2VyZ2V5NzMiLCJhIjoiY2lyM3JhNXR1MDAydGh6bWM3ZzBjaGlrYyJ9.MxdICo0uhxAtmyWpA_CeVw';
     this.map = new this.mapbox.Map({
-      container: 'map',
+      container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v9'
     });
   }
@@ -27,10 +27,11 @@ class App extends React.Component {
   }
 
   render() {
-    this.mapContainer = <div id="map" className="App__map-container" />;
+    // this.mapContainer = <div id="map" className="App__map-container" />;
     return (
       <div className="App">
-        { this.mapContainer }
+        {/* { this.mapContainer } */}
+        <div ref={el => this.mapContainer = el} className="App__map-container" />
       </div>
     );
   }
